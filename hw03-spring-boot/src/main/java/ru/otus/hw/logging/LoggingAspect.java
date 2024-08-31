cee8c2bd3ea624fa5823a2220a1651f4e006445e
Ekaterina Riazanova <kat.riazanowa@yandex.ru>
2024-08-31 11:49:17 +0300

    HW03: Add ConditionalOnProperty to turn off the Logging with aspect, redo .gitignore, add tests

--- hw03-spring-boot/src/main/java/ru/otus/hw/logging/LoggingAspect.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/logging/LoggingAspect.java
@@ -7,11 +7,13 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Aspect
+@ConditionalOnProperty(name = "application.isLoggingAspectEnabled", havingValue = "true")
 @Component
 public class LoggingAspect {
     private static final Logger LOG = LoggerFactory.getLogger(LoggingAspect.class);
